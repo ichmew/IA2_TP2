d3feaf588967076ebe4d0c39322dcd7258c40cad
ichmew <31658773+ichmew@users.noreply.github.com>
2019-05-07 18:49:28 -0300

    Update lll.pl

--- lll.pl
+++ lll.pl
@@ -1,12 +1,11 @@
-threshold_limit is 8. % [mm]
 
 rusting(body, X) :- X = 1.
 rusting(pipes, X) :- X = 1.
 rusting(joints, X) :- X = 1.
 
-less_thickness(body, X) :- X < threshold_limit.
-less_thickness(pipes, X) :- X < threshold_limit.
-less_thickness(joints, X) :- X < threshold_limit.
+less_thickness(body, X) :- X < 10.
+less_thickness(pipes, X) :- X < 8.
+less_thickness(joints, X) :- X < 6.
 
 
 parts(body).
