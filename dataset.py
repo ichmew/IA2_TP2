cd72de70a18dac760397c4ead9cf65027ed31c0e
Sebastian <gzsebas@hotmail.com>
2019-06-04 20:22:04 -0300

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+numfilas=890
+datos = pd.read_csv('titanic_train.csv', header=0, na_filter=False)
+
+datosfiltrados=[datos['Survived'],datos['Pclass'],datos['Sex'],datos['Age'],datos['Embarked']]
+datostranspuestos = np.transpose(datosfiltrados)
+print(datostranspuestos)
+if datostranspuestos[19][3]== '':
+    print('esto es nan')
+
+
+datostranspuestosfiltrados = [v for v in datostranspuestos if v[3] != '']
+
+print(datostranspuestosfiltrados)
+
+print (datostranspuestosfiltrados[1][2])
